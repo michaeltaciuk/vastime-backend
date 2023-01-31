5b3ada22a14a699b5d15c40507930f066b756957
Michael Taciuk <taciuk73@gmail.com>
2023-01-31 15:06:12 -0500

    added x-origin permission for vastime.io

--- server.js
+++ server.js
@@ -17,6 +17,12 @@ const port = process.env.PORT || 3000;
 
         const router = require('./routes');
 
+        app.use(function(req, res, next) {
+            res.header("Access-Control-Allow-Origin", "https://vastime.io");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            next();
+        });          
+
         app.use(bodyParser.json());
         app.use(bodyParser.urlencoded({ extended: true }));
         app.use('/api', router);
